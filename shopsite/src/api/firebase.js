170a3d914e8a81570a925322a09f591b50552b9e
bishoe01 <bishoe01@gmail.com>
2022-12-26 00:36:05 +0900

    google Login 구현

---
+++ shopsite/src/api/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export function login() {
+    signInWithPopup(auth, provider).then((result) => {
+        const user = result.user;
+        console.log(user);
+        // ...
+    }).catch((error) => {
+        console.log(error);
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // const email = error.customData.email;
+        
+    });
+
+}
--- shopsite/src/components/Header.jsx
+++ shopsite/src/components/Header.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import '../App.scss';
 import { FiLogIn } from 'react-icons/fi';
 import { useNavigate, Link } from 'react-router-dom';
+import {AiOutlineShopping} from 'react-icons/ai';
+import {HiOutlinePencilAlt} from 'react-icons/hi';
+import { login } from '../api/firebase';
 function Header(props) {
     const navigate = useNavigate();
     return (
-        <header>
-            <div className='header_content'>
-                <div className='left_container'>
-                    <img className='logo' src="./img/shop_logo.png" alt="mainLogo" />
-                    <h1>비슈상점</h1>
-                </div>
-                <div className='empty'></div>
-                <ul className='right_container'>
+        <header className='flex justify-between border-b border-gray-300 p-2'>
+                    <Link to={'/'} className="flex items-center text-4xl text-brand">
+                        <AiOutlineShopping/>
+                        <h1>비슈상점</h1>
+                        </Link>
+                <nav className='flex items-center gap-4 font-semibold'>
                     <Link to={'/products'}>Products</Link>
                     <Link to={'/carts'}>Carts</Link>
-                    <Link to={'/login'}>Login</Link>
-                    <li><FiLogIn /></li>
-                </ul>
-            </div>
+                    <Link to={'products/new'}><HiOutlinePencilAlt /></Link>
+                    <button onClick={() => login()}>Login</button>
+                </nav>
         </header>
     );
 }
