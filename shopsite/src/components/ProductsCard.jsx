7c6def09526a7a819422a08ea77b74e8ef545f3e
bishoe01 <bishoe01@gmail.com>
2022-12-30 01:10:16 +0900

    transition Animation of Card
    마우스 호버 애니메이션 추가

--- shopsite/src/components/ProductsCard.jsx
+++ shopsite/src/components/ProductsCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProductsCard({ product: { id, price, image, category, title } }) {
     return (
-            <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+            <li className='rounded-lg shadow-md overflow-hidden cursor-pointer hover:scale-105 easy-in-out duration-300'>
                 <img className='w-full' src={image} alt={title} />
                 <div className='mt-2 px-2 text-lg flex justify-between items-center'>
                     <h3 className='truncate'>{title}</h3>
